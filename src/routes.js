a2f1655a3b0b8667015d1f0c70fff4f2e557e492
Magda Aderibigbe <magdaaderibigbe@gmail.com>
2019-08-31 00:51:05 +0300

    Change in routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import { Route } from 'react-router';
+import { Route, IndexRoute } from 'react-router';
 import Navigation from './presentational/navigation.component';
+import Contact from '.contact-component.js';
+import NotFound from '.not-found-component.js';
+import Home from '.home-component.js';
 
 export default (
     <Route path='/' component={Navigation}>
+        <IndexRoute component={Home}/>
+        <Route path='contact/' component={Contact}/>
+        <Route path='*' component={NotFound}/>
     </Route>
 );
