feddc7bf6f41c97c5aaf31194a2bdf08a54446a9
Magda Aderibigbe <magdaaderibigbe@gmail.com>
2019-08-30 22:54:29 +0300

    Create routes

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, hashHistory } from 'react-router';
+import { Provider } from 'react-redux';
+import store from './store';
+
+render(
+    <Provider store={store}>
+        <Router history={hashHistory} routes={routes}/>
+    </Provider>,
+    document.getElementById('root')
+);
